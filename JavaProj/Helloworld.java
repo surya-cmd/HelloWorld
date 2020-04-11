7e5daae18aa885e3a2984ddc5ac7efd24fb8f905
surya <suryadoopaguntla@gmail.com>
2020-04-11 22:46:41 +0530

    File added

---
+++ JavaProj/Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld
+{
+	public static void main (String[] args) 
+	{
+		int i;
+		for ( i=1;i<=10;i++ )
+		{
+		System.out.println("Welcome to Java ........."      +i);
+	}
+}
+}
